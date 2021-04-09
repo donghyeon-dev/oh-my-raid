689aa8ecc373f5471ea34c779418c282a2440c6b
박동현 <donghyeondev@gmail.com>
2021-04-09 17:47:35 +0900

    SwaggerConfig base Package 수정 및 ApiInfo 추가

--- src/main/java/com/ohmyraid/config/SwaggerConfig.java
+++ src/main/java/com/ohmyraid/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,9 +21,26 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .produces(getProduceContentTypes())
                 .select()
-                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트 추출
+                .apis(RequestHandlerSelectors.basePackage("com.ohmyraid")) // 현재 RequestMapping으로 할당된 모든 URL 리스트 추출
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    /**
+     * APIINFO 설정
+     * @return
+     */
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Oh-My-Raid REST API", // Title
+                "OMR Project API", // Description
+                "v1", // Version
+                "서비스 약관 URL",
+                "contactName",
+                "License",
+                "localhost:8880"
+        );
     }
 
     private Set<String> getProduceContentTypes() {
