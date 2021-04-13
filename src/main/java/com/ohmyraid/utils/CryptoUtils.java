3ff2d7863689ddd8ed784bc415713c0d3d756b8a
박동현 <donghyeondev@gmail.com>
2021-04-13 11:37:44 +0900

    Cryptoutils 비밀번호 인코딩 Strength 조절

--- src/main/java/com/ohmyraid/utils/CryptoUtils.java
+++ src/main/java/com/ohmyraid/utils/CryptoUtils.java
@@ -1,14 +1,17 @@
 package com.ohmyraid.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CryptoUtils {
 
     public static String encryptPw(String plainPw){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(15);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
         String encodedPw = encoder.encode(plainPw);
+        log.debug("cryptedPw is {}", encodedPw);
         return encodedPw;
     }
 }
