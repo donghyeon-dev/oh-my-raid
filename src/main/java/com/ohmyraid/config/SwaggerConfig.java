4638ed9e00a4ee626920d65e85fe1a721516e837
박동현 <donghyeondev@gmail.com>
2021-04-27 17:33:32 +0900

    SwaggerUI에 Authorization Header 추가

--- src/main/java/com/ohmyraid/config/SwaggerConfig.java
+++ src/main/java/com/ohmyraid/config/SwaggerConfig.java
@@ -2,14 +2,19 @@ package com.ohmyraid.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -18,8 +23,21 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api(){
+
+        // 스웨거창에 헤더 추가
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder.name("Authorization") // 헤더이름
+                .description("Access Token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .produces(getProduceContentTypes())
+                .globalOperationParameters(parameters)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ohmyraid")) // 현재 RequestMapping으로 할당된 모든 URL 리스트 추출
                 .paths(PathSelectors.any())
