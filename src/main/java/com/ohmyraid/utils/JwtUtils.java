da43e5d2effeeb2f82257a7e6dd593fd1884efe8
박동현 <donghyeondev@gmail.com>
2021-04-27 17:31:38 +0900

    JWT issuer,SecretKey 추가 및 토근만료시간 검증 메서드 추가

--- src/main/java/com/ohmyraid/utils/JwtUtils.java
+++ src/main/java/com/ohmyraid/utils/JwtUtils.java
@@ -1,18 +1,24 @@
 package com.ohmyraid.utils;
 
+import com.ohmyraid.common.result.CommonNoAuthenticationException;
+import com.ohmyraid.common.result.CommonNoSessionException;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Slf4j
 @Component
 public class JwtUtils {
 
-    private String issuer = "ohmyraid";
+    @Value("${spring.jwt.issuer}")
+    private String ISSUER;
+
+    @Value("${spring.jwt.secret-key}")
+    private String SECRET_KEY;
 
     /**
      * Access Token 생성한다.
@@ -23,11 +29,28 @@ public class JwtUtils {
      */
     public String createAccessToken(String id, String name) {
         // Access Token 생성
-        String accessToken = Jwts.builder().setIssuer(issuer).setIssuedAt(new Date())
+        String accessToken = Jwts.builder().setIssuer(ISSUER).setIssuedAt(new Date())
                 .claim("LoginId", id).claim("UserName", name)
-                .signWith(SignatureAlgorithm.HS512,id)
+                .signWith(SignatureAlgorithm.HS512,SECRET_KEY)
                 .setExpiration(new Date(System.currentTimeMillis()+3600000)) //1시간
                 .compact();
         return accessToken;
+    }
+
+    /**
+     * 토큰 만료시간 검사
+     * @param token
+     * @return
+     */
+    public Boolean isTokenValid(String token) {
+        try {
+            final Date expiration = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token)
+                    .getBody().getExpiration();
+            log.debug("JwtUtils :: isTokenExpired return is {}", expiration);
+            return expiration.after(new Date());
+        }catch(ExpiredJwtException e){
+            log.error("Token is Expired");
+            throw new CommonNoSessionException();
+        }
     }
 }
