741dbf239bb1ac9d19cad74d60d217fa442d21c3
박동현 <donghyeondev@gmail.com>
2021-08-04 17:57:57 +0900

    Docker용 client APP에서 접근 가능하게 CORS 추가

--- src/main/java/com/ohmyraid/config/WebMvcConfig.java
+++ src/main/java/com/ohmyraid/config/WebMvcConfig.java
@@ -72,7 +72,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000","http://localhost:5000" );
     }
 
     @Bean
