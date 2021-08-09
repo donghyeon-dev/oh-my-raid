6a1e6e7be70f6baf4ff085a8c766b163bcab1e33
박동현 <donghyeondev@gmail.com>
2021-08-09 16:59:22 +0900

    Entity to DTO 매퍼는 구현됬지만 DBFetch쪽 문제로 DTO to Entity는 미구현 공부 후 적용하자

--- src/main/java/com/ohmyraid/mapper/CharacterMapper.java
+++ src/main/java/com/ohmyraid/mapper/CharacterMapper.java
@@ -2,17 +2,28 @@ package com.ohmyraid.mapper;
 
 import com.ohmyraid.domain.character.CharacterEntity;
 import com.ohmyraid.dto.character.ActualCharacterDto;
-import com.ohmyraid.mapper.resolver.CharacterResolver;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
-@Mapper(uses = {CharacterResolver.class})
+@Mapper//(uses = {CharacterResolver.class})
 public interface CharacterMapper {
 
-    CharacterMapper INSTANCE = Mappers.getMapper(CharacterMapper.class);
-
-    CharacterEntity characterDtoToEntity(ActualCharacterDto characterDto);
+//    CharacterMapper INSTANCE = Mappers.getMapper(CharacterMapper.class);
 
 
+    //    @Mapping(source = "accountId", target = "accountEntity")
+//    CharacterEntity characterDtoToEntity(ActualCharacterDto characterDto, @Context AccountRepository repository);
+
+    @Mapping(target = "accountId", source = "characterEntity.accountEntity.accountId")
+    @Mapping(target = "realm", ignore = true)
     ActualCharacterDto characterEntityToDto(CharacterEntity characterEntity);
+
+//    @ObjectFactory
+//    default AccountEntity findByAccountId(ActualCharacterDto dto, @TargetType Class<AccountEntity> type,
+//                                          @Context AccountRepository repo) {
+//        return !ObjectUtils.isEmpty(dto) && !ObjectUtils.isEmpty(dto.getAccountId())
+//                ? repo.findByAccountId(dto.getAccountId())
+//                : new AccountEntity();
+//    }
+
 }
--- src/main/java/com/ohmyraid/mapper/resolver/CharacterResolver.java
+++ src/main/java/com/ohmyraid/mapper/resolver/CharacterResolver.java
@@ -1,26 +1,26 @@
 package com.ohmyraid.mapper.resolver;
 
-import com.ohmyraid.domain.account.AccountEntity;
 import com.ohmyraid.domain.character.CharacterEntity;
 import com.ohmyraid.dto.character.ActualCharacterDto;
 import com.ohmyraid.repository.account.AccountRepository;
-import lombok.RequiredArgsConstructor;
-import org.mapstruct.ObjectFactory;
+import org.mapstruct.Context;
 import org.mapstruct.TargetType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
 @Component
-@RequiredArgsConstructor
 public class CharacterResolver {
 
-    private final AccountRepository accountRepository;
 
-    @ObjectFactory
-    public AccountEntity resolve(ActualCharacterDto dto, @TargetType Class<CharacterEntity> type) {
-        return !ObjectUtils.isEmpty(dto) && !ObjectUtils.isEmpty(dto.getAccountId()) ?
-                accountRepository.findByAccountId(dto.getAccountId()) :
-                new AccountEntity();
+    @Autowired
+    AccountRepository accountRepository;
+
+    public CharacterEntity findByAccountId(ActualCharacterDto dto, @TargetType Class<CharacterEntity> type,
+                                           @Context AccountRepository repo) {
+        return !ObjectUtils.isEmpty(dto) && !ObjectUtils.isEmpty(dto.getAccountId())
+                ? CharacterEntity.builder().accountEntity(repo.findByAccountId(dto.getAccountId())).build()
+                : new CharacterEntity();
     }
 
 }
