4442bd7595c8442d9b57686ac9f63f48aa304c6d
박동현 <donghyeondev@gmail.com>
2021-08-10 14:48:11 +0900

    Formatting에 의한 코드 변경

--- src/main/java/com/ohmyraid/feign/WowClient.java
+++ src/main/java/com/ohmyraid/feign/WowClient.java
@@ -1,32 +1,31 @@
 package com.ohmyraid.feign;
 
-import com.ohmyraid.dto.auth.AuthDto;
-import com.ohmyraid.dto.auth.AuthRequestDto;
-import com.ohmyraid.dto.auth.SpecInfDto;
+import com.ohmyraid.dto.wow_account.SpecInfDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 /**
  * World Of Warcraft API Feign Interface
  */
-@FeignClient(name="WOW", url = "https://kr.api.blizzard.com")
+@FeignClient(name = "WOW", url = "https://kr.api.blizzard.com")
 public interface WowClient {
 
     @GetMapping(value = "/profile/user/wow")
-    Map<String,Object> getAccountProfileSummary(@RequestParam(name = "namespace" , defaultValue = "profile-kr") String namespace,
-                                                @RequestParam(name = "locale", defaultValue = "ko_KR") String locale,
-                                                @RequestParam(name = "access_token") String access_token,
-                                                @RequestParam(name = ":region", defaultValue = "kr") String region);
+    Map<String, Object> getAccountProfileSummary(@RequestParam(name = "namespace", defaultValue = "profile-kr") String namespace,
+                                                 @RequestParam(name = "locale", defaultValue = "ko_KR") String locale,
+                                                 @RequestParam(name = "access_token") String access_token,
+                                                 @RequestParam(name = ":region", defaultValue = "kr") String region);
 
     @GetMapping(value = "profile/wow/character/{slug}/{characterName}")
-    SpecInfDto getCharacterSpecInf(@RequestParam(name = "namespace" , defaultValue = "profile-kr") String namespace,
+    SpecInfDto getCharacterSpecInf(@RequestParam(name = "namespace", defaultValue = "profile-kr") String namespace,
                                    @RequestParam(name = "access_token") String access_token,
                                    @RequestParam(name = "locale", defaultValue = "ko_KR") String locale,
-                                   @PathVariable(name = "slug")String slug,
-                                   @PathVariable(name = "characterName")String characterName
-                                            );
+                                   @PathVariable(name = "slug") String slug,
+                                   @PathVariable(name = "characterName") String characterName
+    );
 
 }
