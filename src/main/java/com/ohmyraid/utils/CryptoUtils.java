bfd592f7a952e029769fdafc9616f148d3123dd6
박동현 <donghyeondev@gmail.com>
2022-01-28 15:00:30 +0900

    BCryptPasswordEncoder 생성자 추출 리팩토링

--- src/main/java/com/ohmyraid/utils/CryptoUtils.java
+++ src/main/java/com/ohmyraid/utils/CryptoUtils.java
@@ -2,41 +2,45 @@ package com.ohmyraid.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.ObjectUtils;
 
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
-@Component
 public class CryptoUtils {
+    /* CryptoUtil은 Bean이 될 필요가 없지 않을까?
+     BCryptPasswordEncoder를 static member varaiable로 선언하고
+     메서드에서 갖다 쓰면 되지 않을까??
+    */
+    static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(8);
 
-    public static String encryptPw(String plainPw){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
-        String encodedPw = encoder.encode(plainPw);
+    public static String encryptPw(String plainPw) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
+        String encodedPw = passwordEncoder.encode(plainPw);
         log.debug("cryptedPw is {}", encodedPw);
         return encodedPw;
     }
 
-    public static boolean isPwVerify(String plaiPw, String encryptedPw){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
-        return encoder.matches(plaiPw,encryptedPw);
+    public static boolean isPwVerify(String plaiPw, String encryptedPw) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
+        return passwordEncoder.matches(plaiPw, encryptedPw);
 
     }
 
     /**
-     * clientId와 clientSercret을 받아 AuthorizationHeader값을 반환
+     * 환경변수로 저장됨 clientId와 clientSercret을 받아 AuthorizationHeader값을 반환
+     *
      * @return
      */
-    public static String getAuthValue(){
+    public static String getAuthValue() {
         String result = "";
         String clientId = System.getenv("BLIZZARD_CLIENT_ID");
         String clientSecret = System.getenv("BLIZZARD_CLIENT_SECRET");
 
-        if( !ObjectUtils.isEmpty(clientId) && !ObjectUtils.isEmpty(clientSecret) ){
-            result = clientId+ ":" +clientSecret;
-            result = "Basic "+ Base64Utils.encodeToString(result.getBytes(StandardCharsets.UTF_8));
+        if (!ObjectUtils.isEmpty(clientId) && !ObjectUtils.isEmpty(clientSecret)) {
+            result = clientId + ":" + clientSecret;
+            result = "Basic " + Base64Utils.encodeToString(result.getBytes(StandardCharsets.UTF_8));
             return result;
         } else {
             return result;
--- src/test/java/com/ohmyraid/utils/CryptoUtilsTest.java
+++ src/test/java/com/ohmyraid/utils/CryptoUtilsTest.java
@@ -1,14 +1,30 @@
 package com.ohmyraid.utils;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CryptoUtilsTest {
+
+    @Autowired
+    CryptoUtils cryptoUtils;
 
     @Test
     void 통과해야하는_getAuthValue() {
         String result = CryptoUtils.getAuthValue();
         assertEquals(result, "Basic Y2Q1ZjJjYzIwZjBlNGJlMDhlMzFhZTk5MzhlNTZiMmQ6MXBlblN0VG9wb2tnUmhNNGZSdEd3ZXoySlV3eWM3SzI=");
+    }
+
+    @Test
+    void encryptPwWithValidation() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(8);
+        String plainPw = "qwe123";
+        String encryptedPw = encoder.encode(plainPw);
+
+        assertTrue(cryptoUtils.isPwVerify(plainPw, encryptedPw));
+
     }
 }
