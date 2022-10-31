99b0bd0a1906bf17480c7f4aa13b53636de18ab7
박동현 <donghyeondev@gmail.com>
2022-10-31 16:23:06 +0900

    getSpecificCharacterRaidInfo() 구현

--- src/main/java/com/ohmyraid/repository/raid/RaidEncounterRepositoryImpl.java
+++ src/main/java/com/ohmyraid/repository/raid/RaidEncounterRepositoryImpl.java
@@ -1,14 +1,19 @@
 package com.ohmyraid.repository.raid;
 
+import com.ohmyraid.domain.character.QCharacterEntity;
 import com.ohmyraid.domain.raid.QRaidEncounterEntity;
 import com.ohmyraid.domain.raid.RaidEncounterEntity;
+import com.ohmyraid.dto.character.CharacterRaidInfoDto;
+import com.ohmyraid.dto.character.QCharacterRaidInfoDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
-public class RaidEncounterRepositoryImpl {
+public class RaidEncounterRepositoryImpl implements RaidEncounterRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
@@ -21,4 +26,25 @@ public class RaidEncounterRepositoryImpl {
         return targetEntity;
     }
 
+    @Override
+    public List<CharacterRaidInfoDto> findEncounterByCharacterId(long characterId) {
+        QRaidEncounterEntity raidEncounterEntity = QRaidEncounterEntity.raidEncounterEntity;
+        QCharacterEntity characterEntity = QCharacterEntity.characterEntity;
+        return queryFactory
+                .select(new QCharacterRaidInfoDto(
+                        raidEncounterEntity.encounterId,
+                        raidEncounterEntity.characterEntity.characterId.as("characterId"),
+                        characterEntity.name.as("characterName"),
+                        characterEntity.slug.as("slugName"),
+                        raidEncounterEntity.difficulty,
+                        raidEncounterEntity.expansionName,
+                        raidEncounterEntity.instanceName,
+                        raidEncounterEntity.lastCrawledAt,
+                        raidEncounterEntity.progress
+                ))
+                .from(raidEncounterEntity)
+                .innerJoin(characterEntity).on(raidEncounterEntity.characterEntity.characterId.eq(characterEntity.characterId)
+                        .and(characterEntity.characterId.eq(characterId)))
+                .fetch();
+    }
 }
--- src/test/java/com/ohmyraid/repository/raid/RaidEncounterRepositoryImplTest.java
+++ src/test/java/com/ohmyraid/repository/raid/RaidEncounterRepositoryImplTest.java
@@ -1,7 +1,12 @@
 package com.ohmyraid.repository.raid;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ohmyraid.domain.character.QCharacterEntity;
+import com.ohmyraid.domain.raid.QRaidEncounterEntity;
 import com.ohmyraid.domain.raid.RaidEncounterEntity;
+import com.ohmyraid.dto.character.CharacterRaidInfoDto;
+import com.ohmyraid.dto.character.QCharacterRaidInfoDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.logging.Logger;
@@ -10,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,6 +33,10 @@ class RaidEncounterRepositoryImplTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+
+
     @Test
     void findByENcounterID_QueryDSL테스트() {
         // 187 NORMAL BattleForAzeroth Crucible of Storms 8/8 1
@@ -34,6 +45,33 @@ class RaidEncounterRepositoryImplTest {
 
 
         assertEquals(targetEntity.getInstanceName(), "Crucible of Storms");
+
+    }
+
+    @Test
+    void getCharacterRaidInfo_querydsl_test() {
+        long characterId = 6;
+        QRaidEncounterEntity raidEncounterEntity = QRaidEncounterEntity.raidEncounterEntity;
+        QCharacterEntity characterEntity = QCharacterEntity.characterEntity;
+
+        List<CharacterRaidInfoDto> raidInfoDtoList = jpaQueryFactory
+                .select(new QCharacterRaidInfoDto(
+                        raidEncounterEntity.encounterId,
+                        raidEncounterEntity.characterEntity.characterId.as("characterId"),
+                        characterEntity.name.as("characterName"),
+                        characterEntity.slug.as("slugName"),
+                        raidEncounterEntity.difficulty,
+                        raidEncounterEntity.expansionName,
+                        raidEncounterEntity.instanceName,
+                        raidEncounterEntity.lastCrawledAt,
+                        raidEncounterEntity.progress
+                ))
+                .from(raidEncounterEntity)
+                .innerJoin(characterEntity).on(raidEncounterEntity.characterEntity.characterId.eq(characterEntity.characterId)
+                        .and(characterEntity.characterId.eq(characterId)))
+                .fetch();
+
+        raidInfoDtoList.forEach(System.out::println);
 
     }
 
