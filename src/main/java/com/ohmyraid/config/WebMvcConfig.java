c03835366d3b1a6e9a1e239e0c3a45d17b4ccb94
donghyeon-dev <donghyeondev@gmail.com>
2023-01-27 16:08:43 +0900

    Prometheus grafana 추가를 위한 작업(진행중)

--- src/main/java/com/ohmyraid/config/WebMvcConfig.java
+++ src/main/java/com/ohmyraid/config/WebMvcConfig.java
@@ -58,7 +58,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5000");
+                .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:9090");
     }
 
     @Bean
